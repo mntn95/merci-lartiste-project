1d4cc4248b425b29723f01aea488d7657e0c181b
mntn95 <mattngtn@gmail.com>
2022-04-27 23:27:50 +0200

    ✨ Added Main image, rdv call, style, fonts, colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./app.scss";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => (
   <div className="app">
     <Header />
+    <Main />
   </div>
 );
 
