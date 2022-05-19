64f45e6cdd20219e4561ccf57eff66aa575219ff
mntn95 <mattngtn@gmail.com>
2022-05-19 18:25:54 +0200

    ✨ Started footer integ

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,15 @@
-const Footer = () => <div className="footer"></div>;
+import { Col, Container, Row } from "react-bootstrap";
+const Footer = () => (
+  <Container>
+    <Row>
+      <Col>11 impasse le verger</Col>
+      <Col xs={10}>Du Mardi au Samedi</Col>
+    </Row>
+    <Row>
+      <Col>95629 Parmain</Col>
+      <Col xs={10}>De 15h à 20h</Col>
+    </Row>
+  </Container>
+);
 
 export default Footer;
