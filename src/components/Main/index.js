561cde9aeed3aaca45b7acbf6ac05ae29ebe4180
mntn95 <mattngtn@gmail.com>
2022-05-26 13:12:01 +0200

    ✨ replaced scss with fela in main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,14 +1,34 @@
+import { useFela } from "react-fela";
+
+import MainPicture from "../../assets/img/mla_img_1_1920x1080.png";
 import Appointment from "./Appointment";
 import Prices from "./Prices";
 
-const Main = () => (
-  <div className="main">
-    {/* Image */}
-    <div className="main-image" />
-    {/* Aside */}
-    <Appointment />
-    <Prices />
-  </div>
-);
+const main = () => ({
+  margin: "4rem 0",
+});
+
+const mainImage = () => ({
+  backgroundImage: `url(${MainPicture})`,
+  backgroundSize: "100% auto",
+  backgroundRepeat: "no-repeat",
+  clear: "both",
+  width: "100%",
+  minHeight: "100vh",
+});
+
+const Main = () => {
+  const { css } = useFela();
+
+  return (
+    <div className={css(main)}>
+      {/* Image */}
+      <div className={css(mainImage)} />
+      {/* Aside */}
+      <Appointment />
+      <Prices />
+    </div>
+  );
+};
 
 export default Main;
