79e49cf9826b5a6ce27203f38423f8c8e380e6d1
mntn95 <mattngtn@gmail.com>
2022-06-16 23:00:52 +0200

    🐛 Switched photo and video

--- src/components/Main/Prices/index.js
+++ src/components/Main/Prices/index.js
@@ -1,22 +1,7 @@
 import { useFela } from "react-fela";
-import Video from "../../../assets/img/merci_artiste.webm";
+import PricePicture from "../../../assets/img/mla_img_2_1920x1078.png";
 import MovingText from "./MovingText";
 import PricesTable from "./PricesTable";
-
-const pricesContainer = () => ({
-  position: "relative",
-  width: "100%",
-  height: "0",
-  padding: "0 0 56.25%",
-});
-
-const video = () => ({
-  position: "absolute",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-});
 
 const topLines = () => ({
   position: "absolute",
@@ -30,16 +15,33 @@ const bottomLines = () => ({
   width: "100%",
 });
 
+const prices = () => ({
+  marginTop: "4rem",
+  position: "relative",
+  width: "100%",
+  height: "0",
+  padding: "0 0 56.25%",
+});
+
+const pricesImage = () => ({
+  backgroundImage: `url(${PricePicture})`,
+  backgroundSize: "100% auto",
+  backgroundRepeat: "no-repeat",
+  clear: "both",
+  width: "100%",
+  minHeight: "112vh",
+  "@media (min-width: 1921px)": {
+    minHeight: "133vh",
+  },
+});
+
 const Prices = () => {
   const { css } = useFela();
 
   return (
     <section>
-      <div className={css(pricesContainer)}>
-        <video loading="lazy" loop autoPlay muted className={css(video)}>
-          <source src={Video} type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
+      <div className={css(prices)}>
+        <div className={css(pricesImage)} />
         <div className={css(topLines)}>
           <MovingText />
           <PricesTable marginBottom="420px" position="top" />
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,30 +1,37 @@
 import { useFela } from "react-fela";
-
-import MainPicture from "../../assets/img/mla_img_1_1920x1080.png";
+import Video from "../../assets/img/merci_artiste.webm";
 import Appointment from "./Appointment";
 import Prices from "./Prices";
+
+const videoContainer = () => ({
+  position: "relative",
+  width: "100%",
+  height: "0",
+  padding: "0 0 56.25%",
+});
+
+const video = () => ({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+});
 
 const main = () => ({
   marginTop: "4rem",
 });
-
-const mainImage = () => ({
-  backgroundImage: `url(${MainPicture})`,
-  backgroundSize: "100% auto",
-  backgroundRepeat: "no-repeat",
-  clear: "both",
-  width: "100%",
-  minHeight: "100vh",
-});
-
 const Main = () => {
   const { css } = useFela();
 
   return (
     <main className={css(main)}>
-      {/* Image */}
-      <div className={css(mainImage)} />
-      {/* Aside */}
+      <div className={css(videoContainer)}>
+        <video loading="lazy" loop autoPlay muted className={css(video)}>
+          <source src={Video} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <Appointment />
       <Prices />
     </main>
