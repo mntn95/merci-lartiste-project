e5e06b2568e507b25cc0b0000729590ba8cb2b0a
mntn95 <mattngtn@gmail.com>
2024-04-06 13:05:47 +0200

    ✨ Add new test price table image

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,7 @@ import { useFela } from "react-fela";
 import Video from "../../assets/img/merci_artiste.webm";
 import VideoMobile from "../../assets/img/compressedMerciLartiste.mp4";
 import Appointment from "./Appointment";
-import Prices from "./Prices";
+import PriceTable from "../../assets/img/affiche_tarifs_prestations_web.png";
 
 import VideoJS from "./videoTest";
 
@@ -39,6 +39,26 @@ const main = {
       width: "100%",
       height: "100%",
     },
+  },
+};
+
+const priceTableImage = {
+  backgroundImage: `url(${PriceTable})`,
+  backgroundRepeat: "no-repeat",
+  clear: "both",
+  width: "100%",
+  "@media (max-width: 767px)": {
+    minHeight: "800px",
+    backgroundSize: "100% auto",
+    backgroundPosition: "center",
+  },
+  "@media (min-width: 768px) and (max-width: 1639px)": {
+    backgroundSize: "100% auto",
+    minHeight: "2200px",
+  },
+  "@media (min-width: 1640px)": {
+    backgroundSize: "100% auto",
+    minHeight: "2400px",
   },
 };
 
@@ -80,7 +100,9 @@ const Main = ({ setAppointmentRef, setPricesRef }) => {
           </video>
         </div>
         <Appointment setAppointmentRef={setAppointmentRef} />
-        <Prices setPricesRef={setPricesRef} />
+        {/*         <Prices setPricesRef={setPricesRef} />
+         */}{" "}
+        <div className={css(priceTableImage)} />
       </main>
     </>
   );
