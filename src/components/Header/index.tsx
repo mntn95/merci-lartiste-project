d5ffd407dc08e825583110268ff8137c037cae90
Mathieu <mattngtn@gmail.com>
2025-10-04 09:42:51 +0200

    ✨ Add demenagement warning

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -74,6 +74,12 @@ const Header: React.FC<HeaderProps> = ({
             stiffness: 50,
           }}
         >
+          <div className="text-bold py-[40px] text-md md:text-xl">
+            <span className="whitespace-pre-line">
+              INFORMATION IMPORTANTE: A partir du 20 Octobre 2025, nous vous
+              accueillerons au 31 rue jean jaures, 60570 Andeville
+            </span>
+          </div>
           <h1 className="text-[30px] lg:text-[40px] font-normal uppercase">
             {bottomNavLabels.title}
           </h1>
